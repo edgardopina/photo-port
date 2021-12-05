5d1ed5d2b8253eeba2a32701d053b8cdd1a33374
edgardopina <edgardopina@yahoo.com>
2021-12-04 18:39:44 -0700

    Added about section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+//! This App.js file is the center of the application.
+//! Think of App.js as THE ROOT COMPONENT or THE WRAPPER COMPONENT that houses all of the other components.
+//* To effect any change on the application, we need to either modify this file or add components inside it.
+//* React components follow the PascalCase naming convention. Knowing this will help you quickly identify
+//* whether a function is a component or just a regular function.
 
+import React from 'react'; //* we need to import React in every component file
+// import logo from './logo.svg';
+// import './App.css';
+import About from './components/About/index.js';
+
+//* because we are using webpack (in the background) and React, we can add HTML to Javascript without a string
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div>
+         <main>
+            <About>
+            </About>
+         </main>
+      </div>
+   );
 }
 
 export default App;
