67fdd7201c7e5dbbd76bcfff5eaf14939763d034
edgardopina <edgardopina@yahoo.com>
2021-12-09 00:24:46 -0700

    Added Modal component; Updated PhotoList component to conditionally render Modal component

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 function PhotoList({ category }) {
    const [photos] = useState([
@@ -99,24 +100,38 @@ function PhotoList({ category }) {
       },
    ]);
 
+   //! React hook for modal rendering
+   const [currentPhoto, setCurrentPhoto] = useState();
+   
+   //! React hook to manage if Modal is open or not (INSTEAD OF CHECKING FOR onClick event)
+   const [isModalOpen, setIsModalOpen] = useState(false);
+   
    //* photos from current category only
-   const currentPhotos = photos.filter(photo => photo.category === category);
-
-   //* alt - this attribute is used for accessibility user-assistance devices, such as screen readers, so the image's name was assigned.
-   //* key - this attribute was also assigned the image's name. This attribute value must be a unique string. The absence of this unique key
-   //*       value will cause an error message.For more information about using the key attribute, refer to the React documentation on lists
-   //*       and keys(Links to an external site.).
+   const currentPhotos = photos.filter((photo) => photo.category === category);
+   
+   const toggleModal = (image, i) => {
+      setCurrentPhoto({ ...image, index: i }); //* passes image properties and index i
+      setIsModalOpen(true); //! image was clicked on
+      
+   };
+   
+   //* alt - used for accessibility user-assistance devices, such as screen readers, so the image's name was assigned.
+   //* key - also assigned with the image's name. Its value must be a unique string. The absence of this unique key
+   //*       value will cause an error message. Refer to the React documentation on lists and keys.
    //* src - was assigned the require expression. Though this isn't a common practice, it certainly has its use cases! We were also able to
    //*       take advantage of the incremental naming of the images by using i.
-   //* default - this property is where the image has been saved. To render the image, the default property must be invoked.
+   //! default - THIS PROPERTY IS NEEDED TO INDICATE WHERE THE IMAGE WAS SAVED. To render the image, the default property MUST BE invoked.
    return (
       <div>
+         {/* Modal component render ONLY IF isModelOpen === true (need to check for onClick event on the image) */}
+         {isModalOpen && <Modal currentPhoto={currentPhoto} />}
          <div className='flex-row'>
             {currentPhotos.map((image, i) => (
                <img
                   src={require(`../../assets/small/${category}/${i}.jpg`).default}
                   alt={image.name}
                   className='img-thumbnail mx-1'
+                  onClick={() => toggleModal(image, i)}
                   key={image.name}
                />
             ))}
